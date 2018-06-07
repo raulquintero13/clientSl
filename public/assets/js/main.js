fb1b55340e257503b4a77e00e5067742136e7ca7
Raul Quintero <raulquintero13@gmail.com>
2018-06-07 10:13:39 -0700

    web worker

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,6 +1,31 @@
 document.getElementById('id').focus();
 
 
+// WebWorker ///////////////////////////////
+//
+// if(typeof(Worker) !== "undefined") {
+//     if(typeof(w) == "undefined") {
+//         w = new Worker("demo_workers.js");
+//     }
+//     w.onmessage = function(event) {
+//         document.getElementById("result").innerHTML = event.data;
+//     };
+// } else {
+//     document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Workers...";
+// }
+//
+//
+// function stopWorker() { 
+//     w.terminate();
+//     w = undefined;
+// }
+//
+///////////////////////////////////
+
+
+
+
+
 // buttonAdd = document.getElementById("add");
 
 
